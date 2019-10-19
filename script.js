90f92db4ba14216c1db53c5b6c88caeb0f623ce9
Scott Taylor <scottntaylor21@gmail.com>
2019-10-19 10:18:34 -0700

    Time-based color code and local storage added

--- script.js
+++ script.js
@@ -12,24 +12,54 @@ function time(){
     var s = t.getSeconds();
     var m = t.getMinutes();
     var h = t.getHours();
-    $("#date").html(h + ":" + m + ":" + s + " " + date);
+    $("#date").html("The Time is " + h + ":" + m + ":" + s + " " + "and Today is " + date);
  }
  setInterval(time,1000);
 
  time();
 
-
-// liveTime();
-
-//$("#date").html(dateTime);
+// var textinput = document.getElementsByClassName("textinput")
 
 
-// function createEvent() {
-// prompt("What event would you like to add?")
-// }
-// $(".eventSpace").on("click", createEvent);
+ function saveToStorage(textinput){
+     localStorage.setItem("event", JSON.stringify(textinput))
+   console.log(JSON.parse(localStorage.getItem("event")))
+ }
 
-// var el = document.getElementById('txt');
-// el.ondblclick = function(){
-//   this.removeAttribute('readonly');
-// };
+
+ $("#saveBtn").on("click",function(){
+    var textinput = ($(this).prev().find("textarea").val());
+    // //  console.log($(this).prev().val());
+     saveToStorage(textinput);
+    $(this).siblings(".inputText").children("textinput").html(" " + textinput);
+     console.log($(this).prev().find("textarea").val());
+
+
+    //  localStorage.getItem("event")
+    //  $(".textinput").html("event");
+ });
+
+ console.log(localStorage)
+
+
+ // Color coding based on time//
+
+ $(".textinput").each(function (el, element) {
+
+    var currentId = parseInt($(this).attr("id"));
+    
+    var curTime = parseInt(today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds());
+    
+    if (currentId === curTime) {
+        $(this).addClass('present');
+    }
+    if (currentId < curTime) {
+        $(this).addClass('past');
+    }
+    if (currentId > curTime) {
+        $(this).addClass('future');
+    }
+});
+;
+
+
